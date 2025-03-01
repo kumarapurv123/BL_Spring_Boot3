958d65a87f9ab09925a20faef029009e283e69e8
kumarapurv123 <91779441+kumarapurv123@users.noreply.github.com>
2025-03-01 23:39:44 +0530

    Loggin App Setup

---
+++ logging_app/src/test/java/org/ajmera/logging_app/LoggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajmera.logging_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
